bf4f6d01b662e55f0b3a153132af7e48fb747ed2
Vishu Verma <visheshbaund@gmail.com>
2026-02-11 20:58:10 +0530

    feat: initialize React app with TypeScript, Tailwind CSS, and with ShadCN UI.

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>QUEST-AI</h1>
+      <Button>Hello world</Button>
+    </div>
+  );
+};
+
+export default App;
